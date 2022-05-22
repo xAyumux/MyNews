42a49775ea5829e64b942a859a623d41e3a29991
DESKTOP-I5BEVNJ\jin_h <jin.codegym1217@gmail.com>
2022-05-22 11:18:08 +0900

    カードの形を固定

--- src/components/Main/Articles.jsx
+++ src/components/Main/Articles.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Box from "@mui/material/Box"
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -9,26 +10,37 @@ export const Articles = (props) => {
 	const article = props.article;
 	console.log(article);
 	return (
-		<a href={article.url}>
-			<Card sx={{ maxWidth: 345 }}>
-				<CardActionArea>
-					<CardMedia
-						component="img"
-						height="140"
-						image="/static/images/cards/contemplative-reptile.jpg"
-						alt="green iguana"
-					/>
-					<CardContent>
-						<Typography gutterBottom variant="h5" component="div">
-							{article.title}
-						</Typography>
-						<Typography variant="body2" color="text.secondary">
-							Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across
-							all continents except Antarctica
-						</Typography>
-					</CardContent>
-				</CardActionArea>
+	<Box 
+	sx={{
+		display: 'flex',
+		flexWrap: 'wrap',
+		borderRadius: '10px',
+		}}>
+			<a href={article.url}>
+			<Card sx={{ width: 500,
+						height: 300,
+						display: 'flex',
+						mr: 5
+						}}>
+				<Box sx={{ display: 'flex' }}>
+					<CardActionArea>
+						<CardContent>
+							<Typography variant="h10" component="div">
+								{article.title}
+							</Typography>
+							<Typography variant="body2" color="text.secondary">
+								{/* {article.content} 内容を入れるならここ*/}
+							</Typography>
+							<CardMedia
+							component="img"
+							height="140"
+							image={article.img}
+							alt="画像"/>{/* 画像を入れるならここ */}
+						</CardContent>
+					</CardActionArea>
+				</Box>
 			</Card>
 		</a>
+	</Box>
 	);
 };
